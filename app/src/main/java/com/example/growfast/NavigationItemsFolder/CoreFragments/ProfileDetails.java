8fe7cab61a243204678d20b053efc2366395e409
Mohd Asim <mohdasim2882@gmail.com>
2020-12-07 11:03:26 +0530

    Day 5: Profile Page without actual implementation of image in circular image view.
    Signed-off-by: Mohd Asim <mohdasim2882@gmail.com>

--- app/src/main/java/com/example/growfast/NavigationItemsFolder/CoreFragments/ProfileDetails.java
+++ app/src/main/java/com/example/growfast/NavigationItemsFolder/CoreFragments/ProfileDetails.java
@@ -9,15 +9,10 @@ import androidx.fragment.app.Fragment;
 
 import com.example.growfast.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileDetails#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ProfileDetails extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -26,17 +21,10 @@ public class ProfileDetails extends Fragment {
     private String mParam2;
 
     public ProfileDetails() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileDetails.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static ProfileDetails newInstance(String param1, String param2) {
         ProfileDetails fragment = new ProfileDetails();
